f2833eb18a1b49c1100b84b36f6f63979e3d48ce
Pedro Mateus <pedro.mateus@maastro.nl>
2021-07-15 16:01:53 +0200

    Refactor the POC for the v6 wrapper

--- v6_cluster_wrapper/README.md
+++ v6_cluster_wrapper/README.md
@@ -4,9 +4,27 @@ The NCDC Memorabel project requires more computational power to certain tasks th
 Being able to run an algorithm in a different machine while using the vantage6 infrastructure requires some adaptations.
 
 ## Solution
+
 Each cohort will have a default docker image consisting of an adaptation from the usual algorithm wrapper used in vantage6.
 This image will have the configurations required to deploy the algorithm in a different machine.
 The algorithm can be generalize and only needs to have a standard way to input and output data, compliant with the wrapper.
 
-## Maastricht
+## Maastricht POC
+
 The Maastricht node will take advantage of a GPU cluster using OpenShift.
+The ``
+The `docker_wrapper` function will communicate with the cluster using the ...
+
+## Testing
+
+### Vantage6 Node
+
+This algorithm wrapper assumes that the vantage6 node running is based on the following docker image `` ().
+The following repository allows to set up a local PHT using vantage6: .
+
+### Creating a new task
+
+Creating a task using the vantage6 client should be similar to:
+
+```python
+```
--- v6_cluster_wrapper/ncdc_maastricht_wrapper/testing/connectio-manual.py
+++ v6_cluster_wrapper/ncdc_maastricht_wrapper/testing/connectio-manual.py
@@ -24,26 +24,36 @@ def get_tasks(input_folder, output_folder, task_id):
             "file": "template-start-up.json",
             "sleep": 20,
             "env": False,
-            #"commands": [['oc', 'cp', input_folder, f'/mnt/data/{input_folder}']]
-            "commands": [['oc', 'cp', input_folder, f'{task_id}:/mnt/data/data.json']]
+            "task_command": [
+                "sh",
+                "-c",
+                f"until [[ -f /mnt/data/data.json ]]; do echo waiting for the input; sleep 10; done;"
+            ],
+            "commands": [['oc', 'cp', '../data.json', f'{task_id}:/mnt/data/data.json']]
+            #"commands": [['oc', 'cp', input_folder, f'{task_id}:/mnt/data/data.json']]
         },
-        # {
-        #     "task": "run-algorithm",
-        #     "description": "Run the main algorithm",
-        #     "file": "template-run-algorithm.json",
-        #     "sleep": 60,
-        #     "env": True,
-        # },
+        {
+            "task": "test-container-app",
+            "description": "Run the main algorithm",
+            "file": "template-run-algorithm.json",
+            "sleep": 30,
+            "env": True,
+        },
         {
             "task": "clear-up-app",
             "description": "Clear up and finish",
             "file": "template-clear-up.json",
             "sleep": 20,
             "env": False,
+            "task_command": [
+                "sh",
+                "-c",
+                f"until [[ ! -f /mnt/data/data.json ]]; do echo waiting to clear up; sleep 10; done;"
+            ],
             "commands": [
-                ['oc', 'cp', f'{task_id}:/mnt/data/', output_folder],
+                ['oc', 'cp', f'{task_id}:/mnt/data/output.json', './output.json'],
                 #['oc', 'exec', task_id, 'rm', '-rf', '/mnt/data/data.json']
-                #kubectl exec [POD] -- [COMMAND]
+                ['kubectl', 'exec', task_id, '--', 'rm', '-rf', '/mnt/data/output.json'],
                 ['kubectl', 'exec', task_id, '--', 'rm', '-rf', '/mnt/data/data.json']
             ]
         },
@@ -57,9 +67,12 @@ def run_task(task_id, task_definition):
         template = json.load(json_file)
         template["metadata"]["name"] = task_id
         template["metadata"]["labels"]["task"] = task_id
+        if "task_command" in task_definition:
+            template["spec"]["containers"][0]["command"] = task_definition["task_command"]
         output = oc.create(template)
 
 
+    print("Checking pods available")
     c = oc.selector('pods', labels={"task": task_id, "app": task_definition["task"]})
     obj = c.objects()
 
@@ -71,9 +84,14 @@ def run_task(task_id, task_definition):
     print("Created succesfully")
     container_info = obj[0].as_dict()
 
-    print(container_info['status'])
-    time.sleep(20)
-    print(container_info['status'])
+    #while container_info["status"]["phase"] != "Running" or container_info["status"]["phase"] != "Completed":
+    while container_info["status"]["phase"] == "Pending":
+        print("Waiting for the pod to be ready")
+        time.sleep(10)
+        obj[0].refresh()
+        container_info = obj[0].as_dict()
+        #print(container_info['status'])
+
     if "commands" in task_definition and task_definition["commands"]:
         for command in task_definition["commands"]:
             run_command(command, "Success", "Error")
@@ -97,12 +115,13 @@ def run_task(task_id, task_definition):
     #     raise Exception("test")
 
     c.delete()
+    time.sleep(5)
 
 
 task_id = str(uuid.uuid1())
 tasks = get_tasks(
-    "/Users/admin/Projects/memorabel/v6_cluster_wrapper/data.json",
-    "/Users/admin/Projects/memorabel/v6_cluster_wrapper/ncdc_maastricht_wrapper/test.json",
+    "",
+    "",
     task_id
 )
 for task in tasks:
