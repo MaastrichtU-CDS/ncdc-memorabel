418e95955cd605294a96db6ef0b5553135a99a3e
maariebo <127186776+maariebo@users.noreply.github.com>
2025-04-30 13:29:56 +0200

    IM WLT updated to 1-3 trials

--- use_case_3/src/R/RPC_models_LASA.R
+++ use_case_3/src/R/RPC_models_LASA.R
@@ -268,7 +268,7 @@ RPC_models <- function(df, config, model = "memory", exclude=c()) {
     #used norm scores from ADC for logical memory
     if (c("priority_memory_im_ravlt") %in% colnames(df)) {
       df$priority_memory_im_z <-
-        ((df$priority_memory_im_ravlt - (49.672+ (df$age_cent * -0.247) + (df$age_cent2 * -0.0033) + (df$sex_num * -4.227) + (df$education_low * -3.055) + (df$education_high * 2.496))) / 7.826)
+        ((df$priority_memory_im_ravlt - (25.440 + (df$age_cent * -0.150) + (df$age_cent2 * -0.0016) + (df$sex_num * -2.217) + (df$education_low * -1.699) + (df$education_high * 1.467))) / 4.739)
     } else {
     return(list(
         "error_message" = paste("immediate recall test not found, no z-score transformation possible")
