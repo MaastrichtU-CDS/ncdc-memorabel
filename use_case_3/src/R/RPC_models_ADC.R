6bf7ca30a1f14759669a7e711599600fdcb7e4bd
maariebo <127186776+maariebo@users.noreply.github.com>
2025-08-07 10:26:16 +0200

    Changed WLT back to correct z-score calculation

--- use_case_3/src/R/RPC_models_ADC.R
+++ use_case_3/src/R/RPC_models_ADC.R
@@ -279,18 +279,18 @@ RPC_models_ADC <- function(df, config, model = "memory", exclude=c()) {
 
     #Z-score transformations    
     #Z-score: Memory immediate recall
-    #used van der Elst for 15 WLT (this is the total recall for 3 trials)
-    if (c("priority_memory_im_ravlt") %in% colnames(df)) {
+    #used van der Elst for 15 WLT 
+    if (c("priority_memory_im_15_word_list_correct") %in% colnames(df)) {
+      df <- df[df$priority_memory_im_15_word_list_correct < 16 ,]
       df$priority_memory_im_z <-
-      ((df$priority_memory_im_ravlt - (25.440 + (df$age_cent * -0.150) + (df$age_cent2 * -0.0016) + (df$sex_num * -2.217) + (df$education_low * -1.699) + (df$education_high * 1.467))) / 4.739)
+        ((df$priority_memory_im_15_word_list_correct - (49.672+ (df$age_cent * -0.247) + (df$age_cent2 * -0.0033) + (df$sex_num * -4.227) + (df$education_low * -3.055) + (df$education_high * 2.496))) / 7.826)
       df$priority_memory_im_z <- pmax(pmin(df$priority_memory_im_z, 5), -5)
     } else {
-      return(list(
-        "error_message" = paste("immediate recall test not found, no z-score transformation possible")
-      ))
-    }
-
-
+        return(list(
+          "error_message" = paste("immediate recall test not found, no z-score transformation possible")
+        ))
+      }
+    
     #Memory delayed recall z-transformations
     #used van der Elst for RAVLT
     #used norm scores from ADC for logical memory
