403f3e8aa3ee9bad17243341670b5adcb0be065a
maariebo <127186776+maariebo@users.noreply.github.com>
2026-03-24 11:33:03 +0100

    Update RPC_models_EMIF_twins_CS_stratified_sex.R

--- use_case_3/src/R/RPC_models_EMIF_twins/RPC_models_cross_sectional_EMIF_twins/RPC_models_EMIF_twins_CS_stratified_sex.R
+++ use_case_3/src/R/RPC_models_EMIF_twins/RPC_models_cross_sectional_EMIF_twins/RPC_models_EMIF_twins_CS_stratified_sex.R
@@ -185,6 +185,9 @@ RPC_models_EMIF_AD_cs_strat_sex <- function(df, config, model = "memory", exclud
     df$id <- as.factor(as.character(df$id))
     # df %>% dplyr::mutate_if(is.character, as.factor)
 
+    #Remove two outliers
+    df <- df[df$p_tau != 33.4 & df$p_tau != 111, ]
+
     #Descriptive statistics
     #Count of participants
     dplyr::n_distinct(df$id)
